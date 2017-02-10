8f048d7efd32c181cb5b31114e8e1ddb12813d3c
teddy9110 <a.iles@wlv.ac.uk>
2017-02-10 09:58:11 +0000

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,32 @@
+<?php
+
+	
+function Connection()
+{
+/*
+	$server="mi-linux.wlv.ac.uk";
+		
+	$user="1305057";
+		
+	$pass="mickymouse1";
+		
+	$db="db1305057";
+	   	
+*/
+		
+	$mysqli = new mysqli("mi-linux.wlv.ac.uk", "1305057", "mickymouse1", "database");
+			if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+				}
+				
+		//	echo $mysqli->host_info . "\n";
+		
+		return($mysqli);
+
+	
+	
+}
+
+?>
+
+
