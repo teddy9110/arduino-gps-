3a60b5e96d7a8ef34d80af10975c4eb11e262961
EvilBunny <kmalkin@hotmail.co.uk>
2017-03-13 16:25:31 +0000

    fixed the missing code that used to convert the data stream from csv back into js object

--- googlemapsstuff/addfromfile.js
+++ googlemapsstuff/addfromfile.js
@@ -5,28 +5,30 @@ var infowindow = new google.maps.InfoWindow();
 var marker, i;
 
   function readFile(event) {
-	var aMap = maps.getMap();
-	var file = 'https://mi-linux.wlv.ac.uk/~1305057/pins.txt';
-	       
-	$.ajax({
-           url : "pins.txt",
-           dataType: "text",
-           success : function (data) {
-		for (i = 0; i < data.length; i++) {
-                	marker = new google.maps.Marker({
-	                position: new google.maps.LatLng(data[i]["Lat"], data[i]["Long"]),
-        	        map: aMap
-	                });
-	
-	                google.maps.event.addListener(marker, 'click', (function(marker, i) {
-	                return function() {
-	                        infowindow.setContent(data[i]["Name"]);
-	                        infowindow.open(aMap, marker);
-	                }
-	                })(marker, i));
-	        }               
-           }
-       });
+          var aMap = maps.getMap();
+          var file = 'https://mi-linux.wlv.ac.uk/~1305057/pins.txt';
+          $.ajax({
+             url : "pins.txt",
+             dataType: "text",
+             success : function (data) {
+                var csvString = data;
+                var gpsData = $.csv.toObjects(csvString);
+                for (i = 0; i < gpsData.length; i++) {
+	                console.log("LATITUDE = "+gpsData[i]["Lat"]);
+        	        console.log("LONGITUDE = "+gpsData[i]["Long"]);
+                        marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(gpsData[i]["Lat"], gpsData[i]["Long"]),
+                        map: aMap
+                });
 
+		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	        return function() {
+		        infowindow.setContent(gpsData[i]["Name"]);
+		        infowindow.open(aMap, marker);
+		}
+		})(marker, i));
+		}
+     	     }
+  	});
  }
 
