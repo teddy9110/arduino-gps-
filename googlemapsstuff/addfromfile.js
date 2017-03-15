5eaec3ab500df61d448d7e54fcd33f8041593e11
U-DESKTOP-MA29QTE\Alex <alexiles@wlv.ac.uk>
2017-03-15 21:33:48 +0000

    testing git commandline+fixing

--- googlemapsstuff/addfromfile.js
+++ googlemapsstuff/addfromfile.js
@@ -1,6 +1,6 @@
 var fBrowse = document.getElementById('updateloc');
 fBrowse.addEventListener('click', readFile);
-var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+
 var infowindow = new google.maps.InfoWindow();
 var marker, i;
 
@@ -20,18 +20,18 @@ var marker, i;
 	                //console.log("LATITUDE = "+gpsData[i]["Lat"]);
         	       // console.log("LONGITUDE = "+gpsData[i]["Long"]);
                 //    console.log("ID = "+gpsData[i]["ID"]);
-                  if(data[i]["Flag"]=="1"){
+                  if(gpsdata[i]["Flag"]=="1"){
                         marker = new google.maps.Marker({
                         position: new google.maps.LatLng(gpsData[i]["Lat"], gpsData[i]["Long"]),
                         map: aMap,
-                        icon: iconBase + 'https://mi-linux.wlv.ac.uk/~1305057/blue_MarkerA.PNG'
+                        icon:'https://mi-linux.wlv.ac.uk/~1305057/blue_MarkerA.png'
 
                         });
                               }else{
                                 marker = new google.maps.Marker({
                                 position: new google.maps.LatLng(gpsData[i]["Lat"], gpsData[i]["Long"]),
                                 map: aMap,
-                                icon: iconBase + 'https://mi-linux.wlv.ac.uk/~1305057/brown_MarkerA.PNG'
+                                icon:'https://mi-linux.wlv.ac.uk/~1305057/brown_MarkerA.png'
 
                           });
 
