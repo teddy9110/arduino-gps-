2b566ec8439a412b22822715282a54b4f75ceca5
teddy9110 <a.iles@wlv.ac.uk>
2017-03-15 19:50:28 +0000

    Update pull.php

--- googlemapsstuff/pull.php
+++ googlemapsstuff/pull.php
@@ -6,6 +6,7 @@ $mysqli = mysqli_connect("mi-linux.wlv.ac.uk", "1305057", "mickymouse1", "db1305
 
 
 
+
 if (!$mysqli) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
@@ -13,33 +14,115 @@ if (!$mysqli) {
     exit;
 }
 
-
+$lineB = "\n";
 
 $file = "/home/stud/0/1305057/public_html/store.txt";
+//$time = date("d") ;
+bool file_exists ( string $file )
 
-
+if (file_exists== true){
 $f = fopen($file, 'a'); // Open in write mode.MYSQLI_USE_RESULT
+
 
 if ($result = $mysqli->query("SELECT * FROM Location")) {
 
 	printf("Select returned %d rows.\n", $result->num_rows);
+
+      echo fwrite($f,$lineB);
 		for($i =0; $i <$result->num_rows;$i++){
- 			mysqli_data_seek($result,0);
- 			$row =  mysqli_fetch_row($result);
- 			printf ( $flag = $row[0]);
 
-			echo fwrite($f, $flag);
-      printf ( $flag = $row[1]);
-      echo fwrite($f, $flag);
-      printf ( $flag = $row[2]);
-      echo fwrite($f, $flag);
+         			mysqli_data_seek($result,$i);
 
+         			$row =  mysqli_fetch_row($result);
 
+         			printf ( $flag = $row[0]);
+
+        			echo fwrite($f, $flag);
+
+              echo fwrite($f, ",");
+
+              printf ( $flag = $row[1]);
+
+              echo fwrite($f, $flag);
+
+              echo fwrite($f, ",");
+
+              printf ( $flag = $row[2]);
+
+              echo fwrite($f, $flag);
+
+              echo fwrite($f, ",");
+
+              printf ( $flag = $row[3]);
+
+              echo fwrite($f, $flag);
+
+              echo fwrite($f, ",");
+
+              echo fwrite($f, '\n\ ');
+
+              echo fwrite($f,$lineB);
+
+        //      $time = $row[4];
+        //      printf($time);
 
 		}
     	fclose($f);
-}
+}else{
 
+  echo("failed sql ");
+}
+}else {}
+
+  if ($result = $mysqli->query("SELECT * FROM Location")) {
+
+  	printf("Select returned %d rows.\n", $result->num_rows);
+          echo fwrite($f,"ID,Lat,Long,Flag,");
+          echo fwrite($f, '\n\ ');
+        echo fwrite($f,$lineB);
+  		for($i =0; $i <$result->num_rows;$i++){
+
+           			mysqli_data_seek($result,$i);
+
+           			$row =  mysqli_fetch_row($result);
+
+           			printf ( $flag = $row[0]);
+
+          			echo fwrite($f, $flag);
+
+                echo fwrite($f, ",");
+
+                printf ( $flag = $row[1]);
+
+                echo fwrite($f, $flag);
+
+                echo fwrite($f, ",");
+
+                printf ( $flag = $row[2]);
+
+                echo fwrite($f, $flag);
+
+                echo fwrite($f, ",");
+
+                printf ( $flag = $row[3]);
+
+                echo fwrite($f, $flag);
+
+                echo fwrite($f, ",");
+
+                echo fwrite($f, '\n\ ');
+
+                echo fwrite($f,$lineB);
+
+          //      $time = $row[4];
+          //      printf($time);
+
+  		}
+      	fclose($f);
+  }else{
+
+    echo("failed sql ");
+  }
 
 
  $mysqli->close();
