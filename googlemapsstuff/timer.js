d0f58b8149c7cb600c32da32ebcd27fa5d41a50b
teddy9110 <a.iles@wlv.ac.uk>
2017-03-22 16:03:10 +0000

    fixed timer , added php link so it updates csv file

--- googlemapsstuff/timer.js
+++ googlemapsstuff/timer.js
@@ -1,4 +1,63 @@
 var timer;
+var timer2;
+var infowindow = new google.maps.InfoWindow();
+var marker, i;
+
 function starttimer() {
-    timer = setInterval(readFile,300000 );
+  updatemaps();
+console.log("timerstarted");
+  timer = setInterval(updatemaps,30000 );
+  timer = setInterval(getcsv,29000 );
+}
+
+function getcsv(){
+
+  $.ajax({
+                 type: "GET",
+                 url: "pull.php" ,
+                success : updatemaps()
+
+});
+}
+
+function updatemaps(){
+  console.log("map updated");
+  var aMap = maps.getMap();
+  var file = 'https://mi-linux.wlv.ac.uk/~1305057/store.txt';
+  $.ajax({
+      url: "store.txt",
+      dataType: "text",
+      success: function(data) {
+          var csvString = data;
+          var gpsData = $.csv.toObjects(csvString);
+          for (i = 0; i < gpsData.length; i++) {
+              //console.log("LATITUDE = "+gpsData[i]["Lat"]);
+              // console.log("LONGITUDE = "+gpsData[i]["Long"]);
+              //    console.log("ID = "+gpsData[i]["ID"]);
+              if (gpsData[i]["Flag"] == "1") {
+                  marker = new google.maps.Marker({
+                      position: new google.maps.LatLng(gpsData[i]["Lat"], gpsData[i]["Long"]),
+                      map: aMap,
+                      icon: 'https://mi-linux.wlv.ac.uk/~1305057/panic.png'
+                  });
+              } else {
+                  marker = new google.maps.Marker({
+                      position: new google.maps.LatLng(gpsData[i]["Lat"], gpsData[i]["Long"]),
+                      map: aMap,
+                      icon: 'https://mi-linux.wlv.ac.uk/~1305057/normal.png'
+                  });
+              }
+              google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                      return function() {
+                          infowindow.setContent(gpsData[i]["ID"]);
+                          infowindow.open(aMap, marker);
+                      }
+                  })
+                  (marker, i));
+          }
+      }
+  });
+
+
+
 }
